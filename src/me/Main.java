95ddae81e04b90a9eff8fe0458747bab3d325d97
patrickatcw <pj8392@yahoo.com>
2018-05-21 12:27:45 -0400

    made changes for updating program to now function similar to an on-line shopping cart, can explore this further to make a web-page for shopping as a more elaborate example

---
+++ src/me/Main.java
@@ -0,0 +1,200 @@
+package me;
+/*Modify the program so that adding items to the shopping basket doesn't
+    actually reduce the stock count but, instead, reserves the requested
+    number of items.
+
+    You will need to add a "reserved" field to the StockItem class to store the
+    number of items reserved.
+
+    Items can continue to be added to the basket, but it should not be possible to
+    reserve more than the available stock of any item. An item's available stock
+    is the stock count less the reserved amount.
+
+    The stock count for each item is reduced when a basket is checked out, at which
+    point all reserved items in the basket have their actual stock count reduced.
+
+    Once checkout is complete, the contents of the basket are cleared.
+
+    It should also be possible to "unreserve" items that were added to the basket
+    by mistake.
+
+    The program should prevent any attempt to unreserve more items than were
+    reserved for a basket.
+
+    Add code to Main that will unreserve items after they have been added to the basket,
+    as well as unreserving items that have not been added to make sure that the code
+    can cope with invalid requests like that.
+
+    After checking out the baskets, display the full stock list and the contents of each
+    basket that you created.
+
+    - a good example of a real website shopping cart
+    */
+
+import java.util.Map;
+
+public class Main {
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+        StockItem temp = new StockItem("bread", 0.86, 100);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cake", 1.10, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("car", 12.50, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62.0, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.50, 200);
+        stockList.addStock(temp);
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.50, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.40, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+
+        for(String s: stockList.Items().keySet()) {
+            System.out.println(s);
+        }
+
+        Basket timsBasket = new Basket("Tim");
+
+        sellItem(timsBasket, "car", 1);
+        System.out.println(timsBasket);
+
+        sellItem(timsBasket, "car", 1);
+        System.out.println(timsBasket);
+
+        if(sellItem(timsBasket, "car", 1) != 1) {
+            System.out.println("There are no more cars in stock");
+        }
+
+        sellItem(timsBasket, "spanner", 5);
+        //System.out.println(timsBasket);   //not required b/c this item does not exists
+
+        sellItem(timsBasket, "juice", 4);
+        sellItem(timsBasket, "cup", 12);
+        sellItem(timsBasket, "bread", 1);
+        //System.out.println(timsBasket);
+
+        //System.out.println(stockList);
+
+        //step 18 adding a new basket instance
+        Basket basket = new Basket("customer");
+        sellItem(basket, "cup", 100);
+        sellItem(basket, "juice", 5);
+        removeItem(basket, "cup", 1);   //checking to see if it works
+        System.out.println(basket);
+
+        //step 19 removing some items
+        removeItem(timsBasket, "car", 1);
+        removeItem(timsBasket, "cup", 9);
+        removeItem(timsBasket, "car", 1);
+        System.out.println("cars removed: " + removeItem(timsBasket, "car", 1));  // should not remove any
+
+        System.out.println(timsBasket);
+
+        //step 20 remove all items from timsBasket, should make basket empty
+        removeItem(timsBasket, "bread", 1);
+        removeItem(timsBasket, "cup", 3);
+        removeItem(timsBasket, "juice", 4);
+        removeItem(timsBasket, "cup", 3);
+        System.out.println(timsBasket);
+
+        //step 21 printouts for basket
+        System.out.println("\nDisplay stock list before and after checkout");
+        System.out.println(basket);
+        System.out.println(stockList);
+        checkOut(basket);  //updates quantity in basket
+        System.out.println(basket);
+        System.out.println(stockList);
+
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+        StockItem car = stockList.Items().get("car");
+        if(car != null) {
+            car.adjustStock(2000);
+        }
+        if(car != null) {
+            stockList.get("car").adjustStock(-1000);
+        }
+
+        System.out.println(stockList);
+        //step 22 comment out below just for clearity for results
+        /*for(Map.Entry<String, Double> price: stockList.PriceList().entrySet()) {
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }*/
+
+        //step 23 printout timsbasket
+        checkOut(timsBasket);
+        System.out.println(timsBasket);
+
+        //step 24 run
+
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        //step 12 change sellStock to reserveStock
+        if(stockList./*sellStock*/reserveStock(item, quantity) != 0) {
+            //basket.addToBasket(stockItem, quantity);
+            //return quantity;
+            //step 13 change to...
+            return basket.addToBasket(stockItem, quantity);
+        }
+        return 0;
+    }
+
+    //step 14 remove item method
+    public static int removeItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(basket.removeFromBasket(stockItem, quantity) == quantity) {
+            return stockList.unreserveStock(item, quantity);
+        }
+        return 0;
+    }
+
+    //step 15 checkout method
+    public static void checkOut(Basket basket) {  //passing the customers basket
+        for (Map.Entry<StockItem, Integer> item : basket.Items().entrySet()) {
+            stockList.sellStock(item.getKey().getName(), item.getValue());
+        }
+        basket.clearBasket();
+
+        //step 16 stockitem compareto method, just a comment out to avoid print
+        //result clutter
+
+        //step 17 run
+
+    }
+
+}
+
